8332dbc68af04205c70d31205c1368586b81833c
yeonk1 <wlwlwl1256@naver.com>
2025-02-17 20:59:37 +0900

    fix: 오류 수정

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 
-const isProduction = process.env.VITE_APP_MODE === "production";
-const basename = isProduction ? "/portfolio25" : "/";
-
 export default defineConfig({
   plugins: [react(), svgr()],
-  base: basename,
+  base: "/portfolio25",
 });
