4c15f07291115bddfc822e520d0147d935146415
yeonk1 <wlwlwl1256@naver.com>
2025-02-17 20:45:44 +0900

    fix: 오류 수정

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -5,7 +5,6 @@ import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 const MainPage = () => {
     useEffect(() => {
-        document.title = 'KANG JI YEON || PORTFOLIO'
         const windowH = window.innerHeight;
 
         const handleScroll = () => {
