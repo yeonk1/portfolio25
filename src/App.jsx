c551980b2e0cc2fd19365d146517692f0e005f71
yeonk1 <wlwlwl1256@naver.com>
2025-02-17 20:26:28 +0900

    fix: 오류 수정

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import Footer from "./components/common/Footer";
 import MainPage from "./pages/index";
 import useSmoothScroll from "./assets/js/useSmoothScroll";
 
-// 환경에 따라 basename 설정
-const isProduction = process.env.NODE_ENV === "production";
+// Vite에서 사용하는 환경 변수
+const isProduction = import.meta.env.MODE === "production";
 const basename = isProduction ? "/portfolio25" : "/";
 
 const router = createBrowserRouter(
