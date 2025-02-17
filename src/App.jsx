84654b578695cbbf5cbd38c4b92afbcfbfbbf55e
yeonk1 <wlwlwl1256@naver.com>
2025-02-17 20:25:41 +0900

    fix: 오류 수정

--- src/App.jsx
+++ src/App.jsx
@@ -4,27 +4,33 @@ import Footer from "./components/common/Footer";
 import MainPage from "./pages/index";
 import useSmoothScroll from "./assets/js/useSmoothScroll";
 
-const router = createBrowserRouter([
-    {
-        path: "/",
-        element: (
-            <>
-                <Header />
-                <MainPage />
-                <Footer />
-            </>
-        ),
-    },
-],
+// 환경에 따라 basename 설정
+const isProduction = process.env.NODE_ENV === "production";
+const basename = isProduction ? "/portfolio25" : "/";
+
+const router = createBrowserRouter(
+    [
+        {
+            path: "/",
+            element: (
+                <>
+                    <Header />
+                    <MainPage />
+                    <Footer />
+                </>
+            ),
+        },
+    ],
     {
         future: {
             v7_startTransition: true,
         },
-    });
+    }
+);
 
 const App = () => {
     useSmoothScroll();
-    return <RouterProvider router={router} />;  // basename 추가
+    return <RouterProvider router={router} basename={basename} />;
 };
 
 export default App;
