46af8e2b36871b6822ec8e8894c01e9128fabc3f
yeonk1 <wlwlwl1256@naver.com>
2025-02-17 19:21:02 +0900

    feat: 파비콘 설정

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,5 @@ import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
   plugins: [react(), svgr(),],
-  base: '/portfolio25/',
-  build: {
-    rollupOptions: {
-      input: 'index.html', // 빌드할 HTML 파일 명시
-    },
-  },
+  base: '/',
 });
