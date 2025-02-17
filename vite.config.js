79dba861e54da73f0eb0a8fe4b6e27660aaeb87a
yeonk1 <wlwlwl1256@naver.com>
2025-02-17 19:12:39 +0900

    feat: dist/index 수정 막기

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
+import HtmlPlugin from 'vite-plugin-html';
 
 
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [react(),
+    svgr(),
+    HtmlPlugin({
+      inject: {
+        injectFavicon: '/portfolio25/favicon.png', // 파비콘 경로 수정
+      },
+    }),],
   base: '/portfolio25/',
   build: {
     rollupOptions: {
