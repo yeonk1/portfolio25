83cb99aeab2a437a77980bec81f916961873e146
yeonk1 <wlwlwl1256@naver.com>
2025-02-17 20:39:08 +0900

    fix: 오류 수정

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,9 @@ import Footer from "./components/common/Footer";
 import MainPage from "./pages/index";
 import useSmoothScroll from "./assets/js/useSmoothScroll";
 
-// Vite에서 사용하는 환경 변수
-const isProduction = process.env.NODE_ENV === "production";
+const isProduction = import.meta.env.MODE === "production";
 const basename = isProduction ? "/portfolio25" : "/";
-console.log(process.env.NODE_ENV)
+console.log(import.meta.env.MODE);
 const router = createBrowserRouter(
     [
         {
--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
   plugins: [react(), svgr()],
-  base: process.env.NODE_ENV === "production" ? "/portfolio25/" : "/",
+  base: import.meta.env.MODE === "production" ? "/portfolio25/" : "/",
 });
